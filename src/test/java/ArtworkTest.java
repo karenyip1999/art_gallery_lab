0a9ce6a3e54b99bf55c17247eedebd63a68c3d6b
Karen Yip <karenyip1999@gmail.com>
2024-02-08 17:43:06 +0000

    Completed ArtworkTest class getters and setters

--- src/test/java/ArtworkTest.java
+++ src/test/java/ArtworkTest.java
@@ -1,2 +1,58 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+
 public class ArtworkTest {
+
+    Artwork monalisa;
+    Artist davinci;
+    Artist gogh;
+
+    @BeforeEach
+    public void setUp()
+    {
+        davinci = new Artist("daVinci");
+        gogh = new Artist("gogh");
+        monalisa = new Artwork("Mona Lisa", davinci, 150.97);
+    }
+
+    @Test
+    public void getTitleTest()
+    {
+        assertThat(monalisa.getTitle()).isEqualTo("Mona Lisa");
+    }
+
+    @Test
+    public void setTitleTest()
+    {
+        monalisa.setTitle("Starry Night");
+        assertThat(monalisa.getTitle()).isEqualTo("Starry Night");
+    }
+
+    @Test
+    public void getArtistTest()
+    {
+        assertThat(monalisa.getArtist()).isEqualTo(davinci);
+    }
+
+    @Test
+    public void setArtistTest()
+    {
+        monalisa.setArtist(gogh);
+        assertThat(monalisa.getArtist()).isEqualTo(gogh);
+
+    }
+
+    @Test
+    public void getPriceTest()
+    {
+        assertThat(monalisa.getPrice()).isEqualTo(150.97);
+    }
+
+    @Test
+    public void setPriceTest()
+    {
+        monalisa.setPrice(400.13);
+        assertThat(monalisa.getPrice()).isEqualTo(400.13);
+    }
 }
