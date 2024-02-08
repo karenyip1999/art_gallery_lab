32d49bd91a6c7dc64c3fe2cd57218d4251a7a447
Karen Yip <karenyip1999@gmail.com>
2024-02-08 17:58:57 +0000

    Completed Customer class with tests of getters and setters

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,36 @@
 public class Customer {
+
+    private String name;
+    private double wallet;
+
+    public Customer(String inputName, double inputWallet)
+    {
+        this.name = inputName;
+        this.wallet = inputWallet;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+
+    public double getWallet()
+    {
+        return this.wallet;
+    }
+
+    public void setWallet(double amount)
+    {
+        this.wallet = amount;
+    }
+
+
+
+
+
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,41 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+
 public class CustomerTest {
+
+    Customer customer;
+
+    @BeforeEach
+    public void setUp(){
+        customer = new Customer("Emily",500.50);
+    }
+
+    @Test
+    public void getNameTest()
+    {
+        assertThat(customer.getName()).isEqualTo("Emily");
+    }
+
+    @Test
+    public void setNameTest()
+    {
+        customer.setName("John");
+        assertThat(customer.getName()).isEqualTo("John");
+    }
+
+    @Test
+    public void getWalletTest()
+    {
+        assertThat(customer.getWallet()).isEqualTo(500.50);
+    }
+
+    @Test
+    public void setWalletTest()
+    {
+        customer.setWallet(600.50);
+        assertThat(customer.getWallet()).isEqualTo(600.50);
+    }
+
+
 }
