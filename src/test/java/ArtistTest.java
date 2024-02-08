1b16b978378851d4235a4d86840a3f0bdfe39872
Karen Yip <karenyip1999@gmail.com>
2024-02-08 17:03:13 +0000

    ArtistTest class getters and setters complete

--- src/test/java/ArtistTest.java
+++ src/test/java/ArtistTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+
 public class ArtistTest {
+
+    Artist artist;
+
+    @BeforeEach
+    public void setUp()
+    {
+        artist = new Artist("daVinci");
+    }
+
+    @Test
+    public void getNameTest()
+    {
+        assertThat(artist.getName()).isEqualTo("daVinci");
+    }
+
+    @Test
+    public void setNameTest()
+    {
+        artist.setName("picasso");
+        assertThat(artist.getName()).isEqualTo("picasso");
+    }
+
+
+
 }
