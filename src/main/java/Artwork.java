d9218ace0cdc940ccc05bb5445e599a250012e44
Karen Yip <karenyip1999@gmail.com>
2024-02-08 17:11:02 +0000

    Complete Artwork class getters and setters

--- src/main/java/Artwork.java
+++ src/main/java/Artwork.java
@@ -3,12 +3,42 @@ public class Artwork {
     private Artist artist;
     private double price;
 
-    public Artwork(String inputTitle, Artist inputArtist, double inputPrice)
-    {
-    this.title = inputTitle;
-    this.artist = inputArtist;
-    this.price = inputPrice;
+    public Artwork(String inputTitle, Artist inputArtist, double inputPrice) {
+        this.title = inputTitle;
+        this.artist = inputArtist;
+        this.price = inputPrice;
 
     }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String newTitle)
+    {
+        this.title = newTitle;
+    }
+
+    public Artist getArtist()
+    {
+        return this.artist;
+    }
+
+    public void setArtist(Artist newArtist)
+    {
+        this.artist = newArtist;
+    }
+
+    public double getPrice()
+    {
+        return this.price;
+    }
+
+    public void setPrice(double newPrice)
+    {
+        this.price = newPrice;
+    }
+
+
 
 }
