14b7aacb2e55c6833afa514976388262228c4a74
Karen Yip <karenyip1999@gmail.com>
2024-02-08 16:41:10 +0000

    setUp created for Gallery

--- src/test/java/GalleryTest.java
+++ src/test/java/GalleryTest.java
@@ -11,16 +11,29 @@ public class GalleryTest {
     @BeforeEach
     public void setUp()
     {
+        //Gallery object
         gallery = new Gallery("Kaya");
-        //monaLisa = new Artwork("Mona Lisa", "daVinci", 150.97);
-        //sunflower = new Artwork("Sunflower","Gogh", 220.53 );
-        //starryNight = new Artwork("Starry Night", "Picasso",435.22);
+        //Artist objects
+        Artist davinci = new Artist ("daVinci");
+        Artist gogh = new Artist ("Gogh");
+        Artist picasso = new Artist ("Picasso");
+        //Artwork objects
+        monaLisa = new Artwork("Mona Lisa", davinci, 150.97);
+        sunflower = new Artwork("Sunflower",gogh, 220.53 );
+        starryNight = new Artwork("Starry Night", picasso,435.22);
     }
 
     @Test
-    public void GetNameTest()
+    public void getNameTest()
     {
         assertThat(gallery.getName()).isEqualTo("Kaya");
+    }
+
+    @Test
+    public void setNameTest()
+    {
+        gallery.setName("Maren");
+        assertThat(gallery.getName()).isEqualTo("Maren");
     }
 
 
